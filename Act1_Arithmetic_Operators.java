be11e5a9de4cf1c011aa5097b276e26360ccfa80
Xyrelle Kenzenloyd B Daigon <126750711+Yxtiefan@users.noreply.github.com>
2025-02-24 23:07:44 +0800

    Add files via upload

---
+++ Act1_Arithmetic_Operators.java
@@ -0,0 +1,25 @@
+package Prelim;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author admin
+ */
+public class Act1_Arithmetic_Operators {
+     public static void main(String[] args) {
+
+        int x = 10; 
+        int y = 4;  
+        int z = 6;  
+
+        // Permi error tabanggg
+        System.out.println(x + "*" + y + "+" + z + "-46"); 
+        System.out.println("(" + x + "-" + y + ")%" + z + "=0"); 
+        System.out.println("(" + x + "+" + y + "+" + z + ")/3=" + ((x + y + z) / 3)); 
+        System.out.println(x + "*" + z + "-(" + y + "*" + y + ")=" + (x * z - (y * y))); 
+    }
+}
