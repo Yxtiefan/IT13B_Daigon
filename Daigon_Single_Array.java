65e464ef81c71765063c40df16c94ae08da2d707
Xyrelle Kenzenloyd B Daigon <126750711+Yxtiefan@users.noreply.github.com>
2025-03-24 12:00:20 +0800

    Add files via upload

---
+++ Daigon_Single_Array.java
@@ -0,0 +1,12 @@
+public class Daigon_Single_Array {
+		public static void main (String[] args){
+        
+        char letters[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G'};
+        
+        //for(char a : letters){for(char a : letters){for(char a : letters){
+        for(char a : letters){
+            System.out.print(a + " , ");
+        }
+        
+    }
+}
